f2b829d827134efe544ced458aa257ac1e4fc9a6
milagresjr <milagrestobjr@gmail.com>
2026-05-05 14:58:16 +0100

    Corrigindo erro de services api

--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -90,7 +90,7 @@ export default function SignUpForm() {
         <div>
           <div className="mb-5 sm:mb-8">
             <h1 className="mb-2 font-semibold text-gray-800 text-title-sm dark:text-white/90 sm:text-title-md">
-              Sign Up
+              Criar nova conta
             </h1>
             {/* <p className="text-sm text-gray-500 dark:text-gray-400">
               Enter your email and password to sign up!
